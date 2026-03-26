461c0b2b5899976f3bd46582e837f00e739244e0
bat-0002 <batashermani@gmail.com>
2026-03-26 17:26:35 -0500

    add localization

---
+++ localization/en-us.lua
@@ -0,0 +1,12 @@
+return {
+    descriptions = {
+        Back = {
+            b_bstuff_none = {
+                name = "None Deck",
+                text = {
+                    "Applies the downsides of every deck"
+                },
+            }
+        }
+    }
+}
