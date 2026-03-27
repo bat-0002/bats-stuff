95e6178d21df7bc41b10461d946ba6632d45fbb1
bat-0002 <batashermani@gmail.com>
2026-03-26 22:47:41 -0500

    completed functionality of none deck

--- src/backs.lua
+++ src/backs.lua
@@ -2,8 +2,8 @@
 SMODS.Back {
     key = "none",
     pos = { x = 0, y = 0 },
-    config = { discards = -1, hands = -2, dollars = -10, extra_hand_bonus = -2, extra_discard_bonus = -1, no_interest = true, consumable_slot = -2, hand_size = -2, ante_scaling = 2, joker_slot = -1, shop_size = -1, voucher = 'magic_trick' },
+    config = { discards = -1, hands = -2, dollars = -10, extra_hand_bonus = -2, extra_discard_bonus = -1, no_interest = true, consumable_slot = -2, joker_slot = -1, shop_size = -1, voucher = 'v_magic_trick' },
     loc_vars = function(self, info_queue, back)
-        return { vars = { self.config.discards, self.config.hands, self.config.dollars, self.config.extra_hand_bonus, self.config.extra_discard_bonus, self.config.joker_slot, self.config.consumable_slot, self.config.hand_size, self.config.ante_scaling, self.config.shop_size,  } }
+        return { vars = { self.config.discards, self.config.hands, self.config.dollars, self.config.extra_hand_bonus, self.config.extra_discard_bonus, self.config.joker_slot, self.config.consumable_slot, self.config.shop_size,  } }
     end,
 }
